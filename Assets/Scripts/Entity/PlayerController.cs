2a0970d58611ccc2d173b3c6917e635f2702c659
Nam Tran <tranngocnam97@yahoo.com>
2014-12-21 20:27:43 -0500

    Added health
    - Functional player health
    - Zombies still can't die correctly

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -3,10 +3,20 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 1.0f;
+	public bool playerEnabled = true;		// Controller enabled
+	public static float playerHealth = 100f;
 
-	private Vector3 entityPosition;
-	private Vector3 direction;
+	public float speed = 1.0f;		// Player movement speed
+	private Vector3 entityPosition;		// Position of the player
+	private Vector3 playerDirection;		// Direction of the player
+	
+	public float cooldown = 10;		// Cooldown on block editing
+	private float count = 0;	// Count of current cooling down
+
+	public GameObject blockToPlace;		// What block to place
+	private Vector3 blockPosition;		// Position to place a block
+	public GameObject destroyable;		// Used for block deletion
+	public bool allowEdit = true;		// Allows the player to place blocks or shoot
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +25,23 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (playerEnabled) {
+			movePlayer ();
+			rotatePlayer ();
+			toggleMouseMode ();
+			if (allowEdit) {
+					addBlock ();
+			} else {
+					if (Input.GetMouseButton (1)) {
+							Shoot ();
+					}
+			}
+		} else {
+		}
+		CheckForDeath ();
+	}
+
+	private void movePlayer() {
 		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
 			rigidbody2D.transform.position += Vector3.up * Time.deltaTime * speed;
 		}
@@ -30,9 +57,60 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
 			rigidbody2D.transform.position += Vector3.right * Time.deltaTime * speed;
 		}
+	}
 
+	private void rotatePlayer() {
 		entityPosition = Camera.main.WorldToScreenPoint (transform.position);
-		direction = Input.mousePosition - entityPosition;
-		rigidbody2D.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg));
+		playerDirection = Input.mousePosition - entityPosition;
+		rigidbody2D.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, Mathf.Atan2 (playerDirection.y, playerDirection.x) * Mathf.Rad2Deg));
+	}
+
+	private void toggleMouseMode() {
+		if (Input.GetKeyDown (KeyCode.LeftControl)) {
+			allowEdit = !allowEdit;
+		}
+	}
+
+	private void addBlock() {
+		if (count > cooldown) {
+			if (Input.GetMouseButton(1) && allowEdit) {
+				createBlock();
+				count = 0;
+			}
+		} else {
+			count += 1f;
+		}
+	}
+
+	private void createBlock() {
+		blockPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		blockPosition.z = 0;
+		Instantiate (blockToPlace, blockPosition, Quaternion.identity);
+	}
+
+	private void Shoot() {
+		float shotDistance = 20;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit2D hit = Physics2D.Raycast (ray.origin, ray.direction, shotDistance);
+
+		if (hit) {
+			if (hit.collider.gameObject.tag.Equals ("Hostile")) {
+				ZombieController zombie = (ZombieController) hit.collider.gameObject.GetComponent ("ZombieController");
+				zombie.TakeDamage(10);
+			}
+		}
+	}
+
+	private void CheckForDeath() {
+		if (playerHealth <= 0f) {
+			playerEnabled = false;
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag.Equals ("Hostile")) {
+			playerHealth -= 10;
+		}
 	}
 }
--- Assets/Scripts/Entity/ZombieController.cs
+++ Assets/Scripts/Entity/ZombieController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class ZombieController : MonoBehaviour {
 
+	public static float zombieHealth = 100f;
+
 	public float speed = 4.0f;
 	public float turnSpeed = 4.0f;
 
@@ -19,21 +21,40 @@ public class ZombieController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		CheckForDeath ();
+		FollowPlayer ();
+		RotateToPlayer ();
+
+	}
+
+	private void FollowPlayer() {
 		// Assigns values to each position vector
 		entityPosition = transform.position;
 		targetPosition = target.transform.position;
-
+		
 		// Determines the direction that the zombie must move towards
 		direction = targetPosition - entityPosition;
 		direction.z = 0;
 		direction.Normalize ();
-
+		
 		// Make the zombie move
 		Vector3 move = direction * speed + entityPosition;
 		transform.position = Vector3.Lerp (entityPosition, move, Time.deltaTime);
+	}
 
+	private void RotateToPlayer() {
 		// Orients the zombie
 		float targetAngle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0, 0, targetAngle), turnSpeed * Time.deltaTime);
+	}
+
+	private void CheckForDeath() {
+		if (zombieHealth <= 0f) {
+			Destroy (gameObject);
+		}
+	}
+
+	public void TakeDamage(float amount) {
+		zombieHealth -= amount;
 	}
 }
