b439d3d0a40ebce01ebd95eb3a168c4ff549516c
katzekitteh <cowanwalls@gmail.com>
2015-01-09 14:36:37 -0500

    Fixed menu code, rearranged menu
    TODO: Debug menu code, as it may not work on all computers

--- Assets/Scripts/Files and Menus/Menu.cs
+++ Assets/Scripts/Files and Menus/Menu.cs
@@ -12,8 +12,8 @@ public class Menu : MonoBehaviour {
 	private bool video = false;
 
 
-	public float musicVolume;
-	public float sfxVolume;
+	private float musicVolume = 50;
+	private float sfxVolume = 50;
 
 	// Use this for initialization
 	void Start () {
@@ -32,16 +32,16 @@ public class Menu : MonoBehaviour {
 			baseMenu = "Main";
 
 			//Creates a button to start the game
-			if(GUI.Button(new Rect((Screen.width/2) - 50, (Screen.height/2) + 25, 100, 50), "Start Game")){
+			if(GUI.Button(new Rect((Screen.width/2) - 50, 50, 100, 50), "Start Game")){
 				//Starts the game by opening the first scene
 			}
 			//Creates a button to enter the options menu
-			if(GUI.Button(new Rect((Screen.width/2) - 50, (Screen.height/2) - 25, 100, 50), "Options")){
+			if(GUI.Button(new Rect((Screen.width/2) - 50, 100, 100, 50), "Options")){
 			   settings = true;
 			   main = false;
 			}
 			//Creates a button to exit the game
-			if(GUI.Button(new Rect((Screen.width/2) - 50, 50, 100, 50), "Exit Game")){
+			if(GUI.Button(new Rect((Screen.width/2) - 50, (Screen.height/2) + 25, 100, 50), "Exit Game")){
 				//Stops the game
 			}
 		}
@@ -66,22 +66,22 @@ public class Menu : MonoBehaviour {
 
 		if (settings) {
 			//Creates a button to enter the keybinding menu
-			if (GUI.Button (new Rect ((Screen.width / 2) - 50, Screen.height, 100, 50), "Keybindings")) {
+			if (GUI.Button (new Rect ((Screen.width / 2) - 50, 0, 100, 50), "Keybindings")) {
 					keys = true;
 					settings = false;
 			}
 			//Creates a button to enter the sound menu
-			if (GUI.Button (new Rect ((Screen.width / 2) - 50, (Screen.height / 2 + 25), 100, 50), "Audio Settings")) {
+			if (GUI.Button (new Rect ((Screen.width / 2) - 50, (Screen.height / 2 - 75), 100, 50), "Audio Settings")) {
 					sound = true;
 					settings = false;
 			}
 			//Creates a button to enter the video menu
-			if (GUI.Button (new Rect ((Screen.width / 2) + 50, 50, 100, 50), "Video Settings")) {
+			if (GUI.Button (new Rect ((Screen.width / 2) - 50, (Screen.height - 150), 100, 50), "Video Settings")) {
 					video = true;
 					settings = false;
 			}
 			//Creates a back button
-			if (GUI.Button (new Rect ((Screen.width / 2) - 150, 50, 100, 50), "Back")) {
+			if (GUI.Button (new Rect ((Screen.width / 2) - 50, (Screen.height - 50), 100, 50), "Back")) {
 					switch (baseMenu) {
 					case "Main":
 							main = true;
@@ -97,20 +97,20 @@ public class Menu : MonoBehaviour {
 
 		if (sound) {
 			//Creates a horizontal slider for music volume
-			GUI.Label (new Rect((Screen.width / 2) - 25, (Screen.height / 2) + 50, 50, 25), "Music Volume");
-			musicVolume = GUI.HorizontalSlider (new Rect ((Screen.width / 2) - 50, (Screen.height / 2) + 5, 100, 10), 50, 0, 100);
+			GUI.Label (new Rect((Screen.width / 2) - 50, (Screen.height / 2) - 25, 150, 25), "Music Volume");
+			musicVolume = GUI.HorizontalSlider (new Rect ((Screen.width / 2) - 50, (Screen.height / 2) + 5, 100, 10), musicVolume, 0, 100);
 
 			//Creates a horizontal slider for SFX volume
-			GUI.Label (new Rect((Screen.width / 2) - 25, (Screen.height / 2) + 25, 50, 25), "Brightness");
-			sfxVolume = GUI.HorizontalSlider (new Rect ((Screen.width / 2) - 50, (Screen.height / 2) - 10, 100, 10), 50, 0, 100);
+			GUI.Label (new Rect((Screen.width / 2) - 50, 50, 150, 25), "SFX Volume");
+			sfxVolume = GUI.HorizontalSlider (new Rect ((Screen.width / 2) - 50, 75, 100, 10), sfxVolume, 0, 100);
 
 			//Creates a save button for the sound options
-			if (GUI.Button (new Rect ((Screen.width / 2) + 50, 50, 100, 50), "Save")) {
+			if (GUI.Button (new Rect ((Screen.width / 2) + 50, Screen.height - 50, 100, 50), "Save")) {
 					//Code to save will need to work xml files
 			}
 
 			//Creates a back button to get to the options menu
-			if (GUI.Button (new Rect ((Screen.width / 2) - 150, 50, 100, 50), "Back")) {
+			if (GUI.Button (new Rect ((Screen.width / 2) - 150, Screen.height - 50, 100, 50), "Back")) {
 					sound = false;
 					settings = true;
 			}
@@ -118,20 +118,16 @@ public class Menu : MonoBehaviour {
 
 		if (video) {
 			//Creates a horizontal slider for FoV, which does not exist because it is top down
-			GUI.Label(new Rect((Screen.width/2) - 75, (Screen.height/2) + 50, 150, 25), "Useless FoV sldier (Impresses critics)");
-			musicVolume = GUI.HorizontalSlider(new Rect((Screen.width/2) - 50, (Screen.height/2) + 5, 100, 10), 50, 0, 100);
-			
-			//Creates a horizontal slider for SFX volume
-			GUI.Label(new Rect((Screen.width/2) - 25, (Screen.height/2) + 25, 50, 25), "SFX Volume");
-			sfxVolume = GUI.HorizontalSlider(new Rect((Screen.width/2) - 50, (Screen.height/2) - 10, 100, 10), 50, 0, 100);
+			GUI.Label(new Rect((Screen.width/2) - 150, 50, 300, 25), "Useless FoV sldier (Impresses critics)");
+			musicVolume = GUI.HorizontalSlider(new Rect((Screen.width/2) - 50, 75, 100, 10), 50, 0, 100);
 
-			//Creates a save button for the sound options
-			if(GUI.Button(new Rect((Screen.width/2) + 50, 50, 100, 50), "Save")) {
+			//Creates a save button for the video options
+			if(GUI.Button(new Rect((Screen.width/2) + 50, Screen.height - 50, 100, 50), "Save")) {
 				//Code to save will need to work xml files
 			}
 			
 			//Creates a back button to get to the options menu
-			if(GUI.Button(new Rect((Screen.width/2) - 150, 50, 100, 50), "Back")) {
+			if(GUI.Button(new Rect((Screen.width/2) - 150, Screen.height - 50, 100, 50), "Back")) {
 				video = false;
 				settings = true;
 			}
